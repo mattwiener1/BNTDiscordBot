bb279b31acfbe5d86170b3e0ad8c8afa5560c376
Matt Wiener <mwiener@outlook.com>
2024-10-13 17:34:24 -0400

    Cleanup program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿public class Program
 {
-    public static Task Main(string[] args) => new Program().MainAsync();
-
     public async Task MainAsync()
     {
         var discordBot = new DiscordBot();
